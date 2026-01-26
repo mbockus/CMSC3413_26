1e06d10d9e171c04e820df1d0777abb0246e0efa
Mike Bockus <mbockus@uco.edu>
2026-01-25 20:36:01 -0600

    Added example run.bat

---
+++ Project1/run.bat
@@ -0,0 +1,4 @@
+:: Navigate to the folder containing the dotnet project that you've been running to start the project locally
+cd Project1.Server 
+:: Execute dotnet with the argument passed to this script. To ensure your project will start successfully, do a clean git clone (clone into a new fresh directory), and execute "run.bat run" from with the Project1 folder.
+dotnet %1
